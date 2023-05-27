aa250b410cd3587930151e3d0f6039368d4f6e29
Christian <gorto005@gmail.com>
2023-05-27 12:22:14 -0600

    ingest ingested

--- db.py
+++ db.py
@@ -8,29 +8,75 @@ from langchain.text_splitter import MarkdownTextSplitter, RecursiveCharacterText
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.docstore.document import Document
 from langchain.document_loaders import ReadTheDocsLoader
+from langchain.chat_models import ChatOpenAI
+from langchain import PromptTemplate, LLMChain
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
 
 load_dotenv()
 
-def ingest_docs(docspage):
-    """Parse and store given documentation in vector db."""
 
-    loader = ReadTheDocsLoader(docspage)
-    raw_docs = loader.load_docs()
+def generate_metadata(doc_content) -> str:
+    """Generate metadata based off of document text."""
+
+    llm = ChatOpenAI(
+        model_name="gpt-3.5-turbo",
+        temperature=0.6,
+        )
+
+    template = "You are a helpful assistant that will generate five highly accurate keywords based on the text you are given. Respond with the keywords, separated by commas. Respond only with the keywords, not with any other text."
+    system_prompt = SystemMessagePromptTemplate.from_template(template)
+    human_template = "{doc}"
+    human_prompt = HumanMessagePromptTemplate.from_template(human_template)
+    chat_prompt = ChatPromptTemplate.from_messages([system_prompt, human_prompt])
+    chain = LLMChain(llm=llm, prompt=chat_prompt)
+
+    return chain.run(doc=doc_content)
+
+def ingest_docs(subdir):
+    """Parse and store given documentation in vector db."""
+    path = os.path.join("docs", subdir)
+    loader = ReadTheDocsLoader(path=path, encoding="utf-8")
+    raw_docs = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=100,
     )
+    documents = text_splitter.split_documents(raw_docs)
+
+    for document in documents:
+        document.metadata['keywords'] = generate_metadata(document.page_content)
+
+    embeddings = OpenAIEmbeddings()
+
+    chromapath = os.path.join(path, "chroma")
+
+    vector_store = Chroma.from_documents(
+        documents,
+        embeddings,
+        collection_name="docs",
+        persist_directory=chromapath,
+    )
+    try:
+        vector_store.persist()
+    except Exception as e:
+        print(f"Error while persisting vector store: {e}")
+
 
 def get_directories():
     """Get all directories in docs."""
-    subdirectories = next(os.walk("docs"))[1]  # get list of subdirectories
+    subdirectories = next(os.walk("docs"))[1]
     output = []
 
     for subdir in subdirectories:
         subdir_path = os.path.join("docs", subdir)
-        if "chroma" in next(os.walk(subdir_path))[1]:  # check if 'chroma' subdirectory exists
-            subdir += ' ✔️'  # append a check emoji if 'chroma' exists
+        has_chroma = "chroma" in next(os.walk(subdir_path))[1]
 
-        output.append(subdir)
-    
+        output.append((subdir, has_chroma))
+
     return output
+
--- main.py
+++ main.py
@@ -1,25 +1,61 @@
 import streamlit as st
 from dl import download_docs
-from db import get_directories
+from db import get_directories, ingest_docs
 
-docspage = st.text_input('Enter the base URL of the documentation:')
+directories = get_directories()
 
-if docspage:
-    with st.spinner('Downloading documentation...'):
-        download_docs(docspage)
-        e = RuntimeError('Invalid documentation URL')
-        st.exception(e)
-    st.success('Documentation downloaded!')
+st.header("Archivist")
 
-# get all directories in docs
-#dropdown of all directories, append a check emoji to the end if there is a chroma directory in that directory,
+with st.sidebar:
 
-docs_choice = st.selectbox('Select a directory:', get_directories())
+    docspage = st.text_input('Enter the base URL of the documentation:')
 
-clicked = st.button('Ingest docs')
+    if docspage:
+        with st.spinner('Downloading documentation...'):
+            download_docs(docspage)
+            e = RuntimeError('Invalid documentation URL')
+            st.exception(e)
+        st.success('Documentation downloaded!')
 
-if clicked:
-    with st.spinner('Ingesting docs...'):
-        ingest_docs(docs_choice)
-    st.success('Docs ingested!')
+    # get all directories in docs
+    #dropdown of all directories, append a check emoji to the end if there is a chroma directory in that directory,
 
+
+    directory_dict = {f"{name} ✔️" if has_chroma else name: name for name, has_chroma in directories}
+
+    docs_choice = st.selectbox('Select a directory:', list(directory_dict.keys()))
+
+
+    clicked = st.button('Ingest docs')
+
+    if clicked:
+        with st.spinner('Ingesting docs...'):
+            original_name = directory_dict[docs_choice]
+            ingest_docs(original_name)
+        st.success('Docs ingested!')
+
+
+chroma_directories = [(name, has_chroma) for name, has_chroma in directories if has_chroma]
+chroma_dict = {f"{name}": name for name, has_chroma in chroma_directories}
+
+docs_scope = st.selectbox('Choose Scope:', list(chroma_dict.keys()))
+
+def get_text():
+    """Get text from user."""
+
+    styl = f"""
+    <style>
+        .stTextInput {{
+        position: fixed;
+        bottom: 3rem;
+        }}
+    </style>
+    """
+    st.markdown(styl, unsafe_allow_html=True)
+
+    input_text = st.text_input("Input Message: ","", key="input")
+    return input_text
+
+
+
+user_input = get_text()
